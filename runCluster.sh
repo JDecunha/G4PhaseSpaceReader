3d1093631f1cf26f16f095f073448e6613229644
Joseph DeCunha <joeydecunha@gmail.com>
2024-04-04 14:14:50 -0500

    New run scripts

---
+++ runCluster.sh
@@ -0,0 +1,1 @@
+./G4PhaseSpaceReader -out ../output/test -mac ../macros/AllHP_test.mac -seed 4 -mt 4 -phasespace /rsrch3/home/radphys_rsch/jdecunha/seadragon2/PhaseSpaces/G3_beamline_10mil.txt -hadronic_inelastic_physics G4HadronPhysicsQGSP_BIC_AllHP
